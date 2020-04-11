a2868ab387a035e755828e288e419cba89714717
Jan Janßen <janssen@mpie.de>
2020-04-11 16:03:53 +0200

    redirect standard error

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ns_run < inputs.test.periodic.MD.lammps
+ns_run < inputs.test.periodic.MD.lammps > run.log 2> run.error
+ns_analyse -M 0.01 -D 0.01 -n 100 test.periodic.MD.lammps.energies  > analysis.txt 2> analysis.error 
